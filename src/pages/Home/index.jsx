063e0c6c67c6174688c705156237d00d93fef42e
Nika Savikina <nikasavikina@Nikas-MacBook-Pro.local>
2019-07-06 20:28:11 +0100

    icons, three boxes, formatting, fontawesome

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Home.css";
-import girl_img from "../../assets/images/girl.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faTruck,
+  faShoppingBag
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = props => (
   <>
@@ -11,6 +16,24 @@ const Home = props => (
           <p className="heading"> Your minimalistic accessory shop</p>
           <button className="title-button">discover now</button>
         </div>
+      </div>
+    </section>
+
+    <section className="menu">
+      <div className="menu-1">
+        <p className="menu-text"> free shipping</p>
+        <p className="menu-text-2">free international shipping</p>
+        <FontAwesomeIcon className="menu-icon" icon={faTruck} />
+      </div>
+      <div className="menu-2">
+        <p className="menu-text"> fast delivery</p>
+        <p className="menu-text-2">2-3 days local delivery</p>
+        <FontAwesomeIcon className="menu-icon" icon={faBell} />
+      </div>
+      <div className="menu-3">
+        <p className="menu-text"> free returns</p>
+        <p className="menu-text-2">one month free return</p>
+        <FontAwesomeIcon className="menu-icon" icon={faShoppingBag} />
       </div>
     </section>
   </>
