901eed486f122c8706b51b9e3eb68a447e2193c3
Nika Savikina <nikasavikina@Nikas-MacBook-Pro.local>
2019-08-17 13:28:09 +0100

    about us text

--- src/constants/products.js
+++ src/constants/products.js
@@ -17,6 +17,11 @@ const products = [
     price: "£30",
     thumb: goldenChain,
     img: goldenChain,
+    shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   },
@@ -29,7 +34,9 @@ const products = [
     thumb: silverChain,
     img: silverChain,
     shippingDescription: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiu"
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
     ],
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
@@ -39,7 +46,7 @@ const products = [
     name: "Solo Gold Ring",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    price: "30£",
+    price: "£30",
     thumb: soloGold,
     img: soloGold,
     shippingDescription: [
@@ -56,6 +63,12 @@ const products = [
     thumb: twoGolden,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    price: "£45",
+      shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     img: twoGolden,
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
@@ -66,8 +79,13 @@ const products = [
     thumb: chainRing,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      price: "£25",
+      shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     img: chainRing,
-
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   },
@@ -75,10 +93,15 @@ const products = [
     id: "silver-leave",
     name: "Silver Leave",
     thumb: silverRing,
+    price: "£30",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     img: silverRing,
-
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   },
@@ -86,10 +109,15 @@ const products = [
     id: "stone-earring",
     name: "Stone",
     thumb: stone,
+    price: "£45",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     img: stone,
-
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   },
@@ -99,8 +127,13 @@ const products = [
     thumb: oval,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    price: "£35",
     img: oval,
-
+    shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   },
@@ -108,10 +141,15 @@ const products = [
     id: "circle-earring",
     name: "Circled",
     thumb: circle,
+    price: "£40",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     img: circle,
-
+    shippingDescription: [
+      "Free standard shipping.",
+      "Delivery in 2 to 4 working days.",
+      "Including free jewellery packaging."
+    ],
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
   }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./About.css";
 
-const About = props => <h1>About Us</h1>;
+const About = props => (
+
+<>
+<section className="hero-img">
+      <div className="border-box">
+      </div>
+</section>    
+            <div className="product-container-two">
+            <h1 className="heading">About Us</h1>
+            <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat </p>
+            </div>
+</>
+);
 
 export default About;
