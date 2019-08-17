46e71bdcbdb69ae8903f3291a646cb9e89e5838c
Nika Savikina <nikasavikina@Nikas-MacBook-Pro.local>
2019-08-17 14:16:48 +0100

    About US text

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import "./About.css";
 
 const About = props => (
-
-<>
-<section className="hero-img">
-      <div className="border-box">
-      </div>
-</section>    
-            <div className="product-container-two">
-            <h1 className="heading">About Us</h1>
-            <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat </p>
-            </div>
-</>
+  <>
+    <section className="hero-img">
+      <div className="border-box" />
+    </section>
+    <div className="about-us">
+      <h1 className="heading">About Us</h1>
+      <p className="text">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat{" "}
+      </p>
+    </div>
+  </>
 );
 
 export default About;
