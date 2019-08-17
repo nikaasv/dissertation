2eca49d3a47ea81f1788d12ad8ddf08a30a24a17
Veronika Savikina <verosja@gmail.com>
2019-08-17 18:23:31 +0100

    contact form

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import "./Contact.css";
 
-const Contact = props => <h1>Contact Us</h1>;
+const Contact = props => (
+  <div className="contact-page">
+    <h1>Contact Us</h1>
+    <div className="row">
+      <h4 className="heading-contact"> We'd love to hear from you!</h4>
+    </div>
+    <div className="row input-container">
+      <div className="styled-input wide">
+        <input type="text" required />
+        <label>Name</label>
+      </div>
+      <div className="styled-input emailInput">
+        <input type="text" required />
+        <label>Email</label>
+      </div>
+      <div className="styled-input phoneNumberInput">
+        <input type="text" required />
+        <label>Phone Number</label>
+      </div>
+      <div className="styled-input wide">
+        <textarea required />
+        <label>Message</label>
+      </div>
+      <div className="submit-btn">Send Message</div>
+    </div>
+  </div>
+);
 
 export default Contact;
