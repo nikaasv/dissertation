0d300d1f80e3a4e2e0e9e75c283c3c04976ccb21
Veronika Savikina <verosja@gmail.com>
2019-08-17 18:47:41 +0100

    contact form updated

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -4,28 +4,32 @@ import "./Contact.css";
 const Contact = props => (
   <div className="contact-page">
     <h1>Contact Us</h1>
-    <div className="row">
+    <div>
       <h4 className="heading-contact"> We'd love to hear from you!</h4>
     </div>
-    <div className="row input-container">
+    <div className="input-container">
       <div className="styled-input wide">
         <input type="text" required />
         <label>Name</label>
       </div>
-      <div className="styled-input emailInput">
+    </div>
+    <div className="input-container middle-input-container">
+      <div className="styled-input email-input">
         <input type="text" required />
         <label>Email</label>
       </div>
-      <div className="styled-input phoneNumberInput">
+      <div className="styled-input phone-number-input">
         <input type="text" required />
         <label>Phone Number</label>
       </div>
+    </div>
+    <div className="input-container">
       <div className="styled-input wide">
         <textarea required />
         <label>Message</label>
       </div>
-      <div className="submit-btn">Send Message</div>
     </div>
+    <div className="submit-btn">Send Message</div>
   </div>
 );
 
