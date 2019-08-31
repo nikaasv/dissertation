f9e0957db234f194b616361c417618be35e328f2
Veronika Savikina <verosja@gmail.com>
2019-08-31 20:27:21 +0100

    images, navigation ,bag

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 import MS from "../../assets/images/MS.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = props => (
   <nav className={`${props.function}-nav`}>
@@ -11,6 +13,7 @@ const Navigation = props => (
     <NavLink to="/about">About Us</NavLink>
     <NavLink to="/contact">Contact Us</NavLink>
     <NavLink to="/products">Products</NavLink>
+    <FontAwesomeIcon className="nav-icon" icon={faShoppingBag} />
   </nav>
 );
 
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 
-const Contact = props => (
-  <div className="contact-page">
-    <h1>Contact Us</h1>
-    <div>
-      <h4 className="heading-contact"> We'd love to hear from you!</h4>
-    </div>
-    <div className="input-container">
-      <div className="styled-input wide">
-        <input type="text" required />
-        <label>Name</label>
+const Contact = props => {
+  const [name, setName] = useState("");
+  const onChangeName = e => setName(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    alert(`Thanks for your message, ${name}\nWe will contact you back shortly`);
+  };
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <div>
+        <h4 className="heading-contact"> We'd love to hear from you!</h4>
       </div>
+      <form onSubmit={onSubmit}>
+        <div className="input-container">
+          <div className="styled-input wide">
+            <input onChange={onChangeName} type="text" required />
+            <label>Name</label>
+          </div>
+        </div>
+        <div className="input-container middle-input-container">
+          <div className="styled-input email-input">
+            <input type="email" required />
+            <label>Email</label>
+          </div>
+          <div className="styled-input phone-number-input">
+            <input type="phone" required />
+            <label>Phone Number</label>
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="styled-input wide">
+            <textarea required />
+            <label>Message</label>
+          </div>
+        </div>
+        <input type="submit" className="submit-btn" value="Send Message" />
+      </form>
     </div>
-    <div className="input-container middle-input-container">
-      <div className="styled-input email-input">
-        <input type="text" required />
-        <label>Email</label>
-      </div>
-      <div className="styled-input phone-number-input">
-        <input type="text" required />
-        <label>Phone Number</label>
-      </div>
-    </div>
-    <div className="input-container">
-      <div className="styled-input wide">
-        <textarea required />
-        <label>Message</label>
-      </div>
-    </div>
-    <div className="submit-btn">Send Message</div>
-  </div>
-);
+  );
+};
 
 export default Contact;
