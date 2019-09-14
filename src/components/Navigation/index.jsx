27c44e7c32ca33b5e863a4a2d5c7c5c843c164f2
Veronika Savikina <verosja@gmail.com>
2019-09-14 22:36:04 +0100

    shopping bag

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,87 +1,84 @@
-import React from 'react'
-import './Navigation.css'
-import { NavLink } from 'react-router-dom'
-import MS from '../../assets/images/MS.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./Navigation.css";
+import { NavLink } from "react-router-dom";
+import MS from "../../assets/images/MS.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import {
   faTwitter,
   faInstagram,
-  faFacebook,
-} from '@fortawesome/free-brands-svg-icons'
-import { connect } from 'react-redux'
+  faFacebook
+} from "@fortawesome/free-brands-svg-icons";
+import { connect } from "react-redux";
 
 const Navigation = ({ navRole, orders }) => (
   <>
     <nav className={`${navRole}-nav desktop`}>
-      <NavLink className='nav-logo' to='/' exact>
-        <img className='logo' src={MS} alt='logo' />
+      <NavLink className="nav-logo" to="/" exact>
+        <img className="logo" src={MS} alt="logo" />
       </NavLink>
-      {navRole === 'main' ? (
+      {navRole === "main" ? (
         <>
-          <NavLink to='/about'>About Us</NavLink>
-          <NavLink to='/contact'>Contact Us</NavLink>
-          <NavLink to='/products'>Products</NavLink>
-          <NavLink to='/shopping-bag'>
-            <FontAwesomeIcon className='nav-icon' icon={faShoppingBag} />
+          <NavLink to="/about">About Us</NavLink>
+          <NavLink to="/contact">Contact Us</NavLink>
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/shopping-bag">
+            <FontAwesomeIcon className="nav-icon" icon={faShoppingBag} />
             {Boolean(orders.length) && (
-              <span className='nav-icon-badge'>{orders.length}</span>
+              <span className="nav-icon-badge">{orders.length}</span>
             )}
           </NavLink>
         </>
       ) : (
         <span>
-          <a href='https://twitter.com'>
-            <FontAwesomeIcon className='soc-media' icon={faTwitter} />
+          <a href="https://twitter.com">
+            <FontAwesomeIcon className="soc-media" icon={faTwitter} />
           </a>
-          <a href='https://facebook.com'>
-            <FontAwesomeIcon className='soc-media' icon={faFacebook} />
+          <a href="https://facebook.com">
+            <FontAwesomeIcon className="soc-media" icon={faFacebook} />
           </a>
-          <a href='https://instagram.com'>
-            <FontAwesomeIcon className='soc-media' icon={faInstagram} />
+          <a href="https://instagram.com">
+            <FontAwesomeIcon className="soc-media" icon={faInstagram} />
           </a>
         </span>
       )}
     </nav>
     <nav className={`${navRole}-nav mobile`}>
-      <NavLink className='nav-logo' to='/' exact>
-        <img className='logo' src={MS} alt='logo' />
+      <NavLink className="nav-logo" to="/" exact>
+        <img className="logo" src={MS} alt="logo" />
       </NavLink>
-      {navRole === 'main' ? (
-        <div id='menuToggle'>
-          <input type='checkbox' />
+      {navRole === "main" ? (
+        <div id="menuToggle">
+          <input type="checkbox" />
           <span></span>
           <span></span>
           <span></span>
-          <ul id='menu'>
-            <NavLink to='/about'>About Us</NavLink>
-            <NavLink to='/contact'>Contact Us</NavLink>
-            <NavLink to='/products'>Products</NavLink>
-            <NavLink to='/shopping-bag'>
-            <FontAwesomeIcon className='nav-icon' icon={faShoppingBag} />
-            {Boolean(orders.length) && (
-              <span className='nav-icon-badge'>{orders.length}</span>
-            )}
-          </NavLink>
+          <ul id="menu">
+            <NavLink to="/about">About Us</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
+            <NavLink to="/products">Products</NavLink>
+            <NavLink to="/shopping-bag">
+              <FontAwesomeIcon className="nav-icon" icon={faShoppingBag} />
+            </NavLink>
           </ul>
         </div>
       ) : (
         <span>
-          <a href='https://twitter.com'>
-            <FontAwesomeIcon className='soc-media' icon={faTwitter} />
+          <a href="https://twitter.com">
+            <FontAwesomeIcon className="soc-media" icon={faTwitter} />
           </a>
-          <a href='https://facebook.com'>
-            <FontAwesomeIcon className='soc-media' icon={faFacebook} />
+          <a href="https://facebook.com">
+            <FontAwesomeIcon className="soc-media" icon={faFacebook} />
           </a>
-          <a href='https://instagram.com'>
-            <FontAwesomeIcon className='soc-media' icon={faInstagram} />
+          <a href="https://instagram.com">
+            <FontAwesomeIcon className="soc-media" icon={faInstagram} />
           </a>
         </span>
       )}
     </nav>
   </>
-)
+);
 
-const mapStateToProps = state => ({ orders: state.shoppingBagReducer.orders })
+const mapStateToProps = state => ({ orders: state.shoppingBagReducer.orders });
 
-export default connect(mapStateToProps)(Navigation)
+export default connect(mapStateToProps)(Navigation);
--- src/pages/ShoppingBag/index.jsx
+++ src/pages/ShoppingBag/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './shoppingBag.css'
-import { connect } from 'react-redux'
-import ShoppingBagItem from '../../components/ShoppingBagItem'
-import { removeItemFromBag } from '../../actions'
+import React from "react";
+import "./shoppingBag.css";
+import { connect } from "react-redux";
+import ShoppingBagItem from "../../components/ShoppingBagItem";
+import { removeItemFromBag } from "../../actions";
 
 export const ShoppingBag = ({ orders, removeItemFromBag }) => {
-  const onRemove = e => removeItemFromBag(e.currentTarget.dataset.index)
+  const onRemove = e => removeItemFromBag(e.currentTarget.dataset.index);
   return (
     <>
       {orders.length ? (
-        <div>
+        <div className="few-items">
           {orders.map((item, index) => (
             <ShoppingBagItem
               key={item.id}
@@ -20,21 +20,21 @@ export const ShoppingBag = ({ orders, removeItemFromBag }) => {
           ))}
         </div>
       ) : (
-        <div>No items in the bag yet</div>
+        <div className="no-items">No items in the bag yet</div>
       )}
     </>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
-  orders: state.shoppingBagReducer.orders,
-})
+  orders: state.shoppingBagReducer.orders
+});
 
 const mapDispatchToProps = dispatch => ({
-  removeItemFromBag: removeItemFromBag(dispatch),
-})
+  removeItemFromBag: removeItemFromBag(dispatch)
+});
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-)(ShoppingBag)
+  mapDispatchToProps
+)(ShoppingBag);
